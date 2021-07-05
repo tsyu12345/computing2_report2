02fd4a07f9bbea88ab2626cb2e30c8c6ff6453b3
syu_asus_Ubuntu <syukunt@icloud.com>
2021-07-06 00:01:03 +0900

    final commit

--- codes/Q1.js
+++ codes/Q1.js
@@ -5,19 +5,21 @@ function setup() {
 }
 
 function draw() {
-  let y_sum = 0;
-  let repeat = 10000;
-  let days = 365;
-  let count = 0;
+  let y_sum = 0; //365日目の株価の合計値
+  let repeat = 10000; //モンテカルロの試行回数
+  let days = 365; //ランダムウォークのステップ数
+  let count = 0;　//試行回数のカウント（repeatと同じ）
   for(let i = 0; i < repeat; i++) {
-    let y = 10000;
+    let y = 10000;//初期値10000円
     for(let j = 1; j <= days; j++) {
-      y += 200 * randomGaussian() + 5;
+      y += 200 * randomGaussian() + 5;//N(5, 200^{2})より株価の変化量を足す
     }
     y_sum += y;
     count++;
-    console.log(i + "." + y_sum / count);
+    console.log(i + "." + y_sum / count);//1試行あたりの365日目の株価の期待値表示
   }
   textAlign(CENTER, CENTER);
-  text(`${y_sum / count}`, width / 2, height / 2);
+  text(`${y_sum / count}`, width / 2, height / 2);//全体で期待値を算出
 }
+
+
---
+++ codes/Q3.js
@@ -0,0 +1,60 @@
+function setup() {
+    createCanvas(365, 300);
+    noLoop();
+    background(255)
+  }
+  
+  function draw() {
+    let y_sum = 0; //365日目の株価の合計値
+    let y0 = 10000;
+    let x = int(random(1, 2000));//オプション価格の変数
+    let repeat = 10000; //モンテカルロの試行回数
+    let days = 365; //ランダムウォークのステップ数
+    let count = 0;　//試行回数のカウント（repeatと同じ）
+    let values = Array(10000);//ヒストグラム描画用に365日目の株価の値を格納
+    let kaite_return = 0;
+    for(let i = 0; i < repeat; i++) {
+      let y = 10000;//初期値10000円
+      for(let j = 1; j <= days; j++) {
+        y += 200 * randomGaussian() + 5;//N(5, 200^{2})より株価の変化量
+      }
+      if(y <= 0){
+        kaite_return = x-(y-y0)
+      } else {
+        kaite_return = x;
+      }
+      y_sum += y;
+      values[i] = y;
+      count++;
+      console.log(i + "." + y_sum / count);//1試行あたりの365日目の株価の期待値表示
+    }
+    console.log(min(values) + ", " + max(values));
+    let bines = createBins(values, min(values), max(values), 200);
+    drawHistogram(bines);
+    textAlign(CENTER, CENTER);
+    text(`${y_sum / count}`, width / 2, height / 2);//全体で期待値を算出
+  }
+  
+  function createBins(values, minValue, maxValue, numBins) {
+    const bins = new Array(numBins);
+    bins.fill(0);
+    for (let i = 0; i < values.length; ++i) {
+      const j = int(((values[i] - minValue) * numBins) / (maxValue - minValue));
+      if (0 <= j && j < numBins) {
+        bins[j] += 1;
+      }
+    }
+    return bins;
+  }
+  
+  function drawHistogram(bins) {
+    const binWidth = width / bins.length;
+    const binMax = max(bins);
+    fill(0, 0, 255);
+    for (let i = 0; i < bins.length; ++i) {
+      const binHeight = (height * bins[i]) / binMax;
+      rect(binWidth * i, height - binHeight, binWidth, binHeight);
+    }
+  }
+  
+  
